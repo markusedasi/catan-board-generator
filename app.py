698fb8bb20ea355a8bc82ce76d7152daad224cf3
markusedasi <markus.edasi@outlook.com>
2025-11-02 18:24:39 +0200

    Added logic to render on index

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from generator_logic import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    tile_set = generate_board()
+    structured_board = structure_board(tile_set)
+    return render_template("index.html", board=structured_board)
 
 if __name__ == '__main__':
     app.run(debug=True)
