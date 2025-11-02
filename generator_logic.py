9522f1ab870965e93469c9bc5b0dfa7c792e1b52
markusedasi <markus.edasi@outlook.com>
2025-11-02 18:18:08 +0200

    Added board structuring

--- generator_logic.py
+++ generator_logic.py
@@ -12,21 +12,38 @@ TILES = (
 NUMBERS = [2, 3, 3, 4, 4, 5, 5, 6, 6,
             8, 8, 9, 9, 10, 10, 11, 11, 12]
 
+LAYOUT = [3, 4 , 5, 4, 3]
+
 def generate_board():
     tiles = TILES.copy()
     numbers = NUMBERS.copy()
 
-    board = []
+    result = []
 
     random.shuffle(tiles)
     random.shuffle(numbers)
 
     for tile in tiles:
         if tile == "desert":
-            board.append({"tile": tile, "number": None})
+            result.append({"tile": tile, "number": None})
         else:
-            board.append({"tile": tile, "number": numbers.pop()})
-    return board
+            result.append({"tile": tile, "number": numbers.pop()})
+    return result
+
+def structure_board(tile_set):
+    board_layout = LAYOUT.copy()
+    result = []
+    index = 0
+
+    for number_of_tiles in board_layout:
+        row = tile_set[index:index + number_of_tiles]
+        result.append(row)
+        index += number_of_tiles
+    return result
 
 if __name__ == '__main__':
-    print(generate_board())
+    board = generate_board()
+    structured = structure_board(board)
+
+    print(board)
+    print(structured)
